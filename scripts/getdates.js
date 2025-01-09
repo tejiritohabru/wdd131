b8af11eac5405c7264f7d047aa9029c34d812cb2
tejiritohabru <tejiritohabru04@gmail.com>
2025-01-09 10:29:35 +0100

    i created my week 1 assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Get the last modified date of the document
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
